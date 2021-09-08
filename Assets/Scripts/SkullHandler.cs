d27ec59447f470adea93988f01e8d8f445636bc1
TechnoGecko <cr5697@gmail.com>
2021-09-08 15:40:16 -0700

    Small fixes (tiles should show)

--- Assets/Scripts/SkullHandler.cs
+++ Assets/Scripts/SkullHandler.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SkullHandler : MonoBehaviour
 {
     private float skulls = 0;
+
+    public TextMeshProUGUI skullCount;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Skull"))
         {
             skulls++;
-            Destroy(other.gameObject);
+            skullCount.text = skulls.ToString();
+            
             Debug.Log("Got'im");
             
+            Destroy(other.gameObject);
         }
     }
 }
