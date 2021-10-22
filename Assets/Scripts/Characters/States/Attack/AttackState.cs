bd627eeae784b722dd39ddc73acb23ea9c1efb72
TechnoGecko <cr5697@gmail.com>
2021-10-21 23:57:31 -0700

    Small update
    Not many noticeable changes, have to figure out what's wrong with the attack scripts I've got right now before I apply them.

---
+++ Assets/Scripts/Characters/States/Attack/AttackState.cs
@@ -0,0 +1,15 @@
+namespace Characters.States.Attack
+{
+    public abstract class AttackState : CharacterState
+    {
+        public override bool CanTurn => false;
+
+        public override bool CanExitState => false;
+
+        public override void OnExitState()
+        {
+            base.OnExitState();
+            Character.Animancer.EndHitSequence();
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/States/Attack/PlayerAttackState.cs
@@ -0,0 +1,95 @@
+using System;
+using PlatformerGameKit;
+using UnityEngine;
+
+namespace Characters.States.Attack
+{
+    public class PlayerAttackState : AttackState
+    {
+        [SerializeField] private AttackTransition[] _GroundAnimations;
+
+        [SerializeField] private AttackTransition[] _GroundUpAnimations;
+
+        
+        private int _CurrentIndex;
+        
+        private bool _Combo;
+        
+        
+        private void Awake()
+        {
+            Action onEnd = OnAnimationEnd;
+
+            for (int i = 0; i < _GroundAnimations.Length; i++)
+            {
+                _GroundAnimations[i].Events.OnEnd += onEnd;
+
+
+                Character.Body.OnGroundedChanged += OnGroundedChanged;
+            }
+        }
+
+        private void OnGroundedChanged(bool isGrounded)
+        {
+            if (Character.StateMachine.CurrentState == this)
+                Character.StateMachine.ForceSetDefaultState();
+        }
+
+        
+        
+        private void OnEnterState()
+        {
+            base.OnEnterState();
+            _CurrentIndex = 0;
+            _Combo = false;
+            PlayAnimation();
+        }
+
+        private void PlayAnimation()
+        {
+            Character.Animancer.EndHitSequence(); 
+            var animation = CurrentAnimations[_CurrentIndex];
+            Character.Animancer.Play(animation);
+        }
+
+        private AttackTransition[] CurrentAnimations
+        {
+            get
+            {
+                if (Character.MovementDirectionY > 0.5f && _GroundUpAnimations.Length > 0)
+                        return _GroundUpAnimations;
+                else
+                    return _GroundAnimations;
+            }
+        }
+
+        public override bool CanExitState
+        {
+            get
+            {
+                if (Character.StateMachine.NextState == this)
+                    _Combo = true;
+
+                return false;
+            }
+        }
+
+        private void OnAnimationEnd()
+        {
+            if (_Combo)
+            {
+                _Combo = false;
+                _CurrentIndex++;
+
+                if (_CurrentIndex < CurrentAnimations.Length)
+                {
+                    PlayAnimation();
+                    return;
+                }
+            }
+
+            Character.StateMachine.ForceSetDefaultState();
+        }
+
+    }
+}
